9cfd22d63bcbc0672ed25d66740d68e7b3c9a19e
Jobaarron <jobaarronmisenas26@gmail.com>
2025-10-02 21:32:19 +0800

    Adjustments DBSeeder/Emails/Layout

--- resources/views/Components/guidance-layout.blade.php
+++ resources/views/Components/guidance-layout.blade.php
@@ -56,7 +56,6 @@
           <li class="nav-item mb-2">
             <a class="nav-link d-flex justify-content-between align-items-center" href="{{ route('guidance.students.index') }}">
               <span><i class="ri-user-line me-2"></i>Student Profiles</span>
-              {{-- <small class="badge bg-success text-white">Active</small> --}}
             </a>
           </li>
           <li class="nav-item mb-2">
@@ -67,43 +66,10 @@
           </li>
           <li class="nav-item mb-2">
             <span class="nav-link disabled d-flex justify-content-between align-items-center">
-              <span><i class="ri-scan-2-line me-2"></i>Facial Recognition</span>
-              <small class="badge bg-light text-dark">Soon</small>
-            </span>
-          </li>
-          <li class="nav-item mb-2">
-            <span class="nav-link disabled d-flex justify-content-between align-items-center">
-              <span><i class="ri-chat-quote-line me-2"></i>Counseling</span>
-              <small class="badge bg-light text-dark">Soon</small>
-            </span>
-          </li>
-          <li class="nav-item mb-2">
-            <span class="nav-link disabled d-flex justify-content-between align-items-center">
-              <span><i class="ri-briefcase-line me-2"></i>Career Advice</span>
-              <small class="badge bg-light text-dark">Soon</small>
-            </span>
-          </li>
-          <li class="nav-item mb-2">
-            <span class="nav-link disabled d-flex justify-content-between align-items-center">
-              <span><i class="ri-bar-chart-line me-2"></i>Analytics & Reports</span>
-              <small class="badge bg-light text-dark">Soon</small>
-            </span>
-          </li>
-          <li class="nav-item mb-2">
-            <span class="nav-link disabled d-flex justify-content-between align-items-center">
               <span><i class="ri-settings-3-line me-2"></i>Settings</span>
               <small class="badge bg-light text-dark">Soon</small>
             </span>
           </li>
-
-          {{-- @can('create_guidance_accounts')
-          <li class="nav-item mb-2">
-            <a class="nav-link" href="{{ route('guidance.create-account') }}">
-              <i class="ri-user-add-line me-2"></i>Create Account
-            </a>
-          </li>
-          @endcan --}}
-
           <li class="nav-item mt-3">
             <form method="POST" action="{{ route('guidance.logout') }}">
               @csrf
@@ -114,6 +80,7 @@
           </li>
         </ul>
       </nav>
+      
     {{ $slot }}
      
     </div>
