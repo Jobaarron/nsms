8dc8f6ed297cd4066d61147ee186a885d1be61a6
Alvinthekings <22-74738@g.batstate-u.edu.ph>
2025-11-04 14:17:35 +0800

    remove the reject button in forwarded case meeting

--- resources/views/admin/forwarded-cases.blade.php
+++ resources/views/admin/forwarded-cases.blade.php
@@ -72,9 +72,6 @@
                                             <button class="btn btn-outline-success btn-sm approve-sanction-btn" data-sanction-id="{{ $sanction->sanction_id }}" title="Approve Sanction">
                                                 <i class="ri-check-line"></i>
                                             </button>
-                                            <button class="btn btn-outline-danger btn-sm reject-sanction-btn" data-sanction-id="{{ $sanction->sanction_id }}" title="Reject Sanction">
-                                                <i class="ri-close-line"></i>
-                                            </button>
                                             <button class="btn btn-outline-warning btn-sm revise-sanction-btn" data-sanction-id="{{ $sanction->sanction_id }}" title="Revise Sanction" data-bs-toggle="modal" data-bs-target="#reviseSanctionModal" data-sanction="{{ $sanction->sanction }}" data-notes="{{ $sanction->notes }}">
                                                 <i class="ri-edit-line"></i>
                                             </button>
