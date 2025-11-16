daff6e353c9e892401d5d3c30bb91f9de012641c
zxiaoyue <estevesprincess39@gmail.com>
2025-11-17 07:12:09 +0800

    smtp variables changes

--- config/mail.php
+++ config/mail.php
@@ -38,9 +38,9 @@ return [
     'mailers' => [
         'smtp' => [
             'transport'  => 'smtp',
-            'host'       => env('MAIL_HOST', 'smtp.mailtrap.io'),
-            'port'       => env('MAIL_PORT', 2525),
-            'encryption' => env('MAIL_ENCRYPTION', 'tls'),
+            'host'       => env('MAIL_HOST', 'smtp.hostinger.com'),
+            'port'       => env('MAIL_PORT', 465),
+            'encryption' => env('MAIL_ENCRYPTION', 'ssl'),
             'username'   => env('MAIL_USERNAME'),
             'password'   => env('MAIL_PASSWORD'),
             'timeout'    => null,
