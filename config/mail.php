0bd97d7abefc75c72e771719f517659b88716e03
zxiaoyue <estevesprincess39@gmail.com>
2025-11-17 08:10:49 +0800

    mail.php adjustment

--- config/mail.php
+++ config/mail.php
@@ -39,12 +39,19 @@ return [
         'smtp' => [
             'transport'  => 'smtp',
             'host'       => env('MAIL_HOST', 'smtp.hostinger.com'),
-            'port'       => env('MAIL_PORT', 465),
+            'port'       => env('MAIL_PORT', 587),
             'encryption' => env('MAIL_ENCRYPTION', 'ssl'),
             'username'   => env('MAIL_USERNAME'),
             'password'   => env('MAIL_PASSWORD'),
             'timeout'    => null,
             'auth_mode'  => null,
+             'stream' => [
+                'ssl' => [
+                'allow_self_signed' => true,
+                'verify_peer' => false,
+                'verify_peer_name' => false,
+            ],
+    ],
         ],
 
         'ses' => [
